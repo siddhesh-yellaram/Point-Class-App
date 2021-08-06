46101094309e11970d62cab89993a25db4c05a63
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:29:04 +0530

    Add files via upload

---
+++ PointClassApp/Model/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointClassApp.Model
+{
+    class Point
+    {
+        private int _x, _y;
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+       public int X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+    }
+}
---
+++ PointClassApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PointClassApp.Model;
+
+namespace PointClassApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(4, 5);
+            UpdatePoints(p1);
+            PrintDetails(p1);
+            Console.ReadLine();
+        }
+        static void UpdatePoints(Point obj)
+        {
+            obj.X = 12;
+            obj.Y = 10;
+        }
+
+        static void PrintDetails(Point obj)
+        {
+            Console.WriteLine("X-axis Point: " + obj.X);
+            Console.WriteLine("Y-axis Point: " + obj.Y);
+        }
+    }
+}
